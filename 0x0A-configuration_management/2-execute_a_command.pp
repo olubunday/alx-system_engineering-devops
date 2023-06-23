0c67f7dbb6464862fb7511e326b4f2105c27084f
olubunday <adeolaolumide9@gmail.com>
2023-06-23 08:37:36 -0700

    Task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,8 @@
 # Manifest that kills 'killmenow' using pkill
 exec { 'killmenow_process':
   command     => 'pkill killmenow',
-  path        => '/usr/bin:/bin', # Update with the correct path if needed
+  path        => '/usr/bin/bash:/bash',
   refreshonly => true,
-  subscribe   => File['/path/to/trigger_file'], # Replace with an actual file triggering the execution
+  subscribe   => File['/Stage[main]/Main/Exec[killmenow]/returns'],
 }
 
