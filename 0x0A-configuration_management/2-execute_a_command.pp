2d839d71ef48a2efa594284f57f71973dc4beb68
olubunday <adeolaolumide9@gmail.com>
2023-06-23 09:08:17 -0700

    Task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # Manifest that kills 'killmenow' using pkill
 exec { 'killmenow_process':
   command     => 'pkill killmenow',
-  path        => '/usr/bin/killmenow',
+  path        => ['/usr/bin', '/bin'],
   refreshonly => true,
 }
 
